50f07a91c480148aba7d4f787604e1a1e629bac7
Karol <karol.zakrzewski@outlook.com>
2024-01-13 19:28:16 +0100

    feat: create payment page

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+"use client";
+import { Button } from "@/components/form/Button";
+import { handleCheckPayment } from "@/lib/payment";
 
 const Payment = ({ searchParams }: any) => {
   console.log(searchParams);
 
-  return <div>Payment - {searchParams.status}</div>;
+  return (
+    <div>
+      Payment - {searchParams.status}
+      <Button
+        onClick={async () => {
+          const x = await handleCheckPayment();
+          console.log("🚀  x:", x);
+        }}
+      >
+        Check payment
+      </Button>
+    </div>
+  );
 };
 
 export default Payment;
---
+++ src/lib/payment/index.ts
@@ -0,0 +1,47 @@
+import { ResponseApi } from "@/lib/types";
+import { getSession } from "next-auth/react";
+
+export const handleCheckPayment = async (): Promise<
+  ResponseApi.Error | ResponseApi.Success<{ url: string }>
+> => {
+  try {
+    const session = await getSession();
+
+    if (!session) {
+      throw Error("Please authenticate. Cannot get auth session");
+    }
+
+    const jwt = session.user.token;
+
+    const res = await fetch(
+      "http://localhost:4000/api/check-payment",
+      // "https://gf-ecommerce.vercel.app/api/check-payment",
+      {
+        method: "POST",
+        cache: "no-store",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${jwt}`,
+        },
+        body: JSON.stringify({ paymentId: "pi_3MtwBwLkdIwHu7ix28a3tqPa" }),
+      },
+    );
+
+    const data = await res.json();
+
+    if (!data) {
+      throw Error("Cannot fetch products in cart");
+    }
+
+    return { data, success: true, error: null };
+  } catch (error) {
+    if (error instanceof Error) {
+      return { data: null, success: false, error: error.message };
+    }
+    return {
+      data: null,
+      success: false,
+      error: "Something went wrong with get cart",
+    };
+  }
+};
